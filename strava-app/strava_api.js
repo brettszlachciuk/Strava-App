6cc9f6e3f15dbb7ce0e1c83d42f605ec69cb490f
Brett Szlachciuk <brettszlachciuk@gmail.com>
2022-05-18 17:15:18 -0400

    change

--- strava-app/strava_api.js
+++ strava-app/strava_api.js
@@ -2,28 +2,31 @@ const auth_link = "https://www.strava.com/oauth/token";
 
 function getActivities(res) {
   const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`;
-
-  fetch(activities_link).then((res) => console.log(res));
+  //const activities_link =
+  //"https://www.strava.com/api/v3/athlete/activities?access_token=26ddfeb56561a1e286a4cc5b4d0d096982de9ecc";
+  fetch(activities_link)
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => console.log(res));
 }
-
-getActivities();
 
 function reAuthorize() {
   fetch(auth_link, {
     method: "post",
     headers: {
-      Accept: "application/json, text/plain, */*",
+      Accept: "application/json",
       "Content type": "application/json",
     },
 
     body: JSON.stringify({
       client_id: "84686",
       client_secret: "9fcbbcdd437f9aa1c5e6146dc4254cd0d59a9829",
-      refresh_token: "cfc19366ca63e850cc6a4fd7aaca324f72864323",
+      refresh_token: "e01a7c5501f97ca459dcac5eeca06a84b998d023",
       grant_type: "refresh_token",
     }),
   })
-    .then((res) => res.json())
+    .then((res) => console.log(res.json()))
     .then((res) => getActivities(res));
 }
 
